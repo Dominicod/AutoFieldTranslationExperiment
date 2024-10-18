737eb74fe17e3ec42a7d6fbfeac52605d9db4eae
dominicod <dominicodonnell99@gmail.com>
2024-10-18 16:30:59 -0500

    Feat: Fetch language from browser and set Culture

--- AutoFieldTranslationExperiment/Middleware/RequestInformationMiddleware.cs
+++ AutoFieldTranslationExperiment/Middleware/RequestInformationMiddleware.cs
@@ -1,10 +1,20 @@
+using System.Globalization;
+
 namespace AutoFieldTranslationExperiment.Middleware;
 
 internal sealed class RequestInformationMiddleware(ILogger<RequestInformationMiddleware> logger) : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        logger.LogInformation("Request: {Method} {Path}", context.Request.Method, context.Request.Path);
+        var request = context.Request;
+        var acceptLanguageHeader = request.Headers.AcceptLanguage;
+        
+        // Fetch the first value from language header, if not found, default to en-US
+        // The first should in theory be the default language of the browser
+        var preferredBrowserLanguage = acceptLanguageHeader.ToString().Split(',').FirstOrDefault() ?? "en-US";
+        Thread.CurrentThread.CurrentCulture = new CultureInfo(preferredBrowserLanguage);
+        
+        logger.LogInformation("Request: {Method} {Path} Language: {LanguageCode}", context.Request.Method, context.Request.Path, preferredBrowserLanguage);
         
         await next(context);
     }
