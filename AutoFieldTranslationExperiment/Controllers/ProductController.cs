e7304f5cf9d0c4c8e68eeb97923cc0bccb092353
dominicod <dominicodonnell99@gmail.com>
2024-10-18 16:01:18 -0500

    Feat: Controller Routes

--- AutoFieldTranslationExperiment/Controllers/ProductController.cs
+++ AutoFieldTranslationExperiment/Controllers/ProductController.cs
@@ -1,3 +1,5 @@
+using Ardalis.GuardClauses;
+using AutoFieldTranslationExperiment.DTOs;
 using AutoFieldTranslationExperiment.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,19 +11,50 @@ public class ProductController(IProductService productService) : ControllerBase
 {
     [HttpGet]
     [Route("")]
-    public async Task<IActionResult> GetAll()
+    public async Task<IResult> GetAll()
     {
         var products = await productService.GetProductsAsync();
-        
-        return Ok(products);
+
+        return Results.Ok(products);
     }
     
     [HttpGet]
     [Route("{id:guid}")]
-    public async Task<IActionResult> Get(Guid id)
+    public async Task<IResult> Get(Guid id)
     {
-        var product = await productService.GetProductAsync(id);
+        try
+        {
+            var product = await productService.GetProductAsync(id);
+            return Results.Ok(product);
+        }
+        catch (NotFoundException)
+        {
+            return Results.NotFound();
+        }
+    }
+    
+    [HttpPost]
+    [Route("")]
+    public async Task<IResult> Create(ProductDto request)
+    {
+        var product = await productService.CreateProductAsync(request);
         
-        return Ok(product);
+        return Results.Created($"/api/products/{product.Id}", product);
+    }
+    
+    [HttpPut]
+    [Route("")]
+    public async Task<IResult> Update(ProductDto request)
+    {
+        try
+        {
+            await productService.UpdateProductAsync(request);
+        }
+        catch (NotFoundException)
+        {
+            return Results.NotFound();
+        }
+
+        return Results.NoContent();
     }
 }
