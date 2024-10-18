f5028e481d3b045f0d3e49008258926fbf3e5423
dominicod <dominicodonnell99@gmail.com>
2024-10-18 16:59:04 -0500

    Feat: Possible first iteration of Translations dynamically being created/updated

--- AutoFieldTranslationExperiment/Services/ProductService.cs
+++ AutoFieldTranslationExperiment/Services/ProductService.cs
@@ -32,7 +32,16 @@ internal sealed class ProductService(IApplicationDbContext context) : IProductSe
 
     public async Task<Product> CreateProductAsync(ProductCreate request)
     {
-        var product = new Product();
+        var product = new Product
+        {
+            NameTranslations = [
+                new Translation
+                {
+                    LanguageCode = Thread.CurrentThread.CurrentCulture.Name,
+                    Value = request.Name
+                }
+            ]
+        };
         
         await context.Products.AddAsync(product);
         await context.SaveChangesAsync();
@@ -41,10 +50,22 @@ internal sealed class ProductService(IApplicationDbContext context) : IProductSe
 
     public async Task<Product> UpdateProductAsync(ProductUpdate request)
     {
-        var product = new Product
-        {
-            Id = request.Id,
-        };
+        var product = await context.Products
+            .Include(i => i.NameTranslations)
+            .FirstOrDefaultAsync(i => i.Id == request.Id);
+        
+        Guard.Against.NotFound("Product", product, nameof(product));
+        
+        var currentTranslation = product.NameTranslations.FirstOrDefault(i => i.LanguageCode == Thread.CurrentThread.CurrentCulture.Name);
+        
+        if (currentTranslation is not null)
+            currentTranslation.Value = request.Name;
+        else
+            product.NameTranslations.Add(new Translation
+            {
+                LanguageCode = Thread.CurrentThread.CurrentCulture.Name,
+                Value = request.Name
+            });
         
         context.Products.Update(product);
         await context.SaveChangesAsync();
