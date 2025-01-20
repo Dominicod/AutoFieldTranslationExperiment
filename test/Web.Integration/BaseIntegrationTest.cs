1c88516a0509ab9b4ef231eac660d9d2bf37237a
dominicod <dominicodonnell99@gmail.com>
2025-01-20 10:05:14 -0600

    Feat: Add HTTP client to integration tests

--- test/Web.Integration/BaseIntegrationTest.cs
+++ test/Web.Integration/BaseIntegrationTest.cs
@@ -8,11 +8,16 @@ public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppF
 {
     private readonly IServiceScope _scope;
     protected readonly IApplicationDbContext _context;
-    
+    protected readonly HttpClient _client;
+
     protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
     {
         // Fetch and assign scopes for use in integration tests
         _scope = factory.Services.CreateScope();
         _context = _scope.ServiceProvider.GetRequiredService<IApplicationDbContext>();
+        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = false
+        });
     }
 }
--- test/Web.Integration/ProductControllerTests.cs
+++ test/Web.Integration/ProductControllerTests.cs
@@ -4,40 +4,39 @@ namespace Web.Test;
 
 public class ProductControllerTests(IntegrationTestWebAppFactory factory) : BaseIntegrationTest(factory)
 {
-    //https://xunit.net/docs/shared-context#class-fixture
     public class GetAllMethod
     {
         [Fact]
         public void Returns_AllProducts()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_EmptyList_WhenNoProducts()
         {
-            
+
         }
     }
-    
+
     public class GetMethod
     {
         [Fact]
         public void Returns_Product_WhenExists()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_NotFound_WhenProductDoesNotExist()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenProductIdIsInvalid()
         {
-            
+
         }
     }
 
@@ -46,19 +45,19 @@ public class ProductControllerTests(IntegrationTestWebAppFactory factory) : Base
         [Fact]
         public void Creates_Product()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenProductAlreadyExists()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenRequestIsInvalid()
         {
-            
+
         }
     }
 
@@ -67,40 +66,40 @@ public class ProductControllerTests(IntegrationTestWebAppFactory factory) : Base
         [Fact]
         public void Updates_Product()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenProductDoesNotExist()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenRequestIsInvalid()
         {
-            
+
         }
     }
-    
+
     public class DeleteMethod
     {
         [Fact]
         public void Deletes_Product()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_NotFound_WhenProductDoesNotExist()
         {
-            
+
         }
-        
+
         [Fact]
         public void Returns_BadRequest_WhenProductIdIsInvalid()
         {
-            
+
         }
     }
 }
